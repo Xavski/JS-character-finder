345317ed7db04631f2e76e38692fd2c5ae39be50
Xavski <xavioursaveski1@gmail.com>
2022-08-11 10:45:17 +1000

    Search bar

--- app.js
+++ app.js
@@ -27,25 +27,37 @@
 
 // renderCharacters();
 
-
-
 const charactersListEl = document.querySelector(".characters-list");
+const searchBar = document.getElementById("searchBar");
+let charactersData = []
+
+
+searchBar.addEventListener("keyup", (e) => {  //key on release
+  const searchString = e.target.value.toLowerCase()
+  const filteredCharacters = charactersData.filter(character => {
+    return character.name.toLowerCase().includes(searchString) || character.house.toLowerCase().includes(searchString)
+  })
+  characterHTML(filteredCharacters)
+  console.log(filteredCharacters)
+});
 
 async function renderCharacters() {
   const characters = await fetch("https://hp-api.herokuapp.com/api/characters");
-  const charactersData = await characters.json();
-  charactersListEl.innerHTML = charactersData
-    .map((character) => characterHTML(character))
-    .join("");
+  charactersData = await characters.json();
+
+  characterHTML(charactersData)
 }
 
 function characterHTML(characters) {
-  return `
-            <div class="character__container">
-                <img src="${characters.image}" class="character__img"></img>
-                <div class="character__name">${characters.name}</div>
-                <div class="character__house-name">${characters.house}</div>
-            </div>
-        `;
+  const htmlString = characters.map((character) =>{
+    return `
+              <div class="character__container">
+                  <img src="${character.image}" class="character__img"></img>
+                  <div class="character__name">${character.name}</div>
+                  <div class="character__house-name">${character.house}</div>
+              </div>
+          `;
+  }).join('')
+  charactersListEl.innerHTML = htmlString
 }
 renderCharacters();
